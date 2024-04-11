1c35f60d3eb7c174b10de53c28805dcf4e07c82f
Serhii Kozik <serhiikozik@Serhiis-MBP.home>
2024-04-11 09:34:37 +0300

    configure testing tools

---
+++ remix.config.js
@@ -0,0 +1,6 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+    appDirectory: "app",
+    ignoredRouteFiles: ["**/*.test{.ts,.tsx}"],
+    assetsBuildDirectory: "public/build",
+  };
